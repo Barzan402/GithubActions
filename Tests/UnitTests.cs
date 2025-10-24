d49c136df76c78a3b97712b34b05e8d18960472b
Barzan402 <bmustafa3@huskers.unl.edu>
2025-10-23 22:41:13 -0500

    Add PowerTests for valid and invalid cases

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,31 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+[TestClass]
+public class PowerTests_Mustafa
+{
+    [TestMethod]
+    public void Power_Valid_Mustafa()
+    {
+        Assert.AreEqual(8, Program.Power("2", "3"));
+        Assert.AreEqual(1, Program.Power("5", "0"));
+        Assert.AreEqual(9, Program.Power("3", "2"));
+    }
+
+    [TestMethod]
+    public void Power_Invalid_Mustafa()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+    }
+
+    [TestMethod]
+    public void Power_Null_Mustafa()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+    }
+}
