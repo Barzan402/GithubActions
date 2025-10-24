1c3fbecfde2744a89ab63b222bb1ae9746d6d698
Barzan402 <bmustafa3@huskers.unl.edu>
2025-10-23 22:31:07 -0500

    Replace Add method tests with Power method tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -6,24 +6,24 @@ public class Addition
 	[TestMethod]
 	public void Add_Valid_Mustafa()
 	{
-		Assert.AreEqual(3, Program.Add("1", "2"));
-		Assert.AreEqual(5, Program.Add("3", "2"));
-		Assert.AreEqual(12, Program.Add("5", "7"));
+		Assert.AreEqual(3, Program.Power("1", "2"));
+		Assert.AreEqual(5, Program.Power("3", "2"));
+		Assert.AreEqual(12, Program.Power("5", "7"));
 	}
 
 	[TestMethod]
 	public void Add_Invalid_Mustafa()
 	{
-		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
 	}
 
 	[TestMethod]
 	public void Add_Null_Mustafa()
 	{
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
 	}
 }
